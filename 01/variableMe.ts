728ad3e39ee45e2f602edb339b36c4cf84270660
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2023-06-07 20:19:07 +0300

    Finished with 01

---
+++ 01/variableMe.ts
@@ -0,0 +1,20 @@
+let hello: string = "Hello!"
+hello.toLocaleLowerCase()
+console.log(hello)
+
+//number
+let userId: number = 316551
+
+//boolean
+let isLoggedIn: boolean = false
+
+//any
+
+let hero: string
+
+function getHero() {
+    return "spidy"
+}
+
+hero = getHero()
+
